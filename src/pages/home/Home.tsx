500427ec13e3be374a5edb7c338e2d2258d7622b
Sandro <sandrolp41@hotmail.com>
2026-01-22 01:49:35 -0300

    criar componente formpostagem - estilização

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
 
 function Home() {
   return (
@@ -13,10 +14,9 @@ function Home() {
               Expresse aqui seus pensamentos e opiniões
             </p>
             <div className="flex justify-around gap-4">
-              <button className="rounded text-white 
-                               border-white border-solid border-2 py-2 px-4">
-                Nova Postagem
-              </button>
+              
+                <ModalPostagem />
+              
             </div>
           </article>
           <figure className="flex justify-center">
