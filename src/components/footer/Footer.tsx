8a7b7a95435f5fe25bcb5275a3ee406e331c07a0
Sandro <sandrolp41@hotmail.com>
2026-01-22 22:52:53 -0300

    feat: implementar ajustes finais

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,34 +1,47 @@
-import {
-  FacebookLogoIcon,
-  InstagramLogoIcon,
-  LinkedinLogoIcon,
-} from "@phosphor-icons/react";
+import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
+import { useContext, type ReactNode } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
 
 function Footer() {
+
   const data = new Date().getFullYear();
 
+  const { usuario } = useContext(AuthContext)
+
+  let component: ReactNode
+
+    if (usuario.token !== "") {
+
+      component = (
+        
+        <footer className="flex justify-center bg-indigo-900 text-white">
+          <div className="container flex flex-col items-center py-4">
+            <p className="text-xl font-bold">
+              Blog Pessoal Generation | Copyright: {data}
+            </p>
+            <p className="text-lg">Acessa nossas redes sociais</p>
+            <div className="flex gap-2">
+              <a href="https://www.linkedin.com/in/sandrocgomes" target="_blank">
+                <LinkedinLogoIcon size={48} weight="bold" />
+              </a>
+              <a href="https://www.linkedin.com/" target="_blank">
+                <InstagramLogoIcon size={48} weight="bold" />
+              </a>
+              <a href="https://www.linkedin.com/" target="_blank">
+                <FacebookLogoIcon size={48} weight="bold" />
+              </a>
+            </div>
+          </div>
+        </footer>
+
+      )
+    }
+
   return (
-    <footer className="flex justify-center bg-indigo-900 text-white">
-      <div className="container flex flex-col items-center py-4">
-        <p className="text-xl font-bold">
-          Blog Pessoal Generation | Copyright: {data}
-        </p>
-
-        <p className="text-lg">Acessa nossas redes sociais</p>
-
-        <div className="flex gap-2">
-          <a href="https://www.linkedin.com/in/sandrocgomes" target="_blank">
-            <LinkedinLogoIcon size={48} weight="bold" />
-          </a>
-          <a href="#" target="_blank">
-            <InstagramLogoIcon size={48} weight="bold" />
-          </a>
-          <a href="#" target="_blank">
-            <FacebookLogoIcon size={48} weight="bold" />
-          </a>
-        </div>
-      </div>
-    </footer>
+    <>
+      { component }
+    </>
   );
 }
 
